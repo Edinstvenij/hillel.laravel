7bbcffa90e800a0206d9216b0fa0bffd12938d38
Yura <yuravigovskij@gmail.com>
2022-11-11 17:22:41 +0200

    DZ-20 finish

--- app/Http/Controllers/Admin/GeoIpController.php
+++ app/Http/Controllers/Admin/GeoIpController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Jobs\ProcessUserAgent;
-use App\Services\Geo\GeoServiceInterface;
-use itHillelDz19\UserAgentInterface\UserAgentInterface;
 
 class GeoIpController
 {
 
     public function index()
     {
-        ProcessUserAgent::dispatch("50.7.93.27", request()->userAgent());
+        ProcessUserAgent::dispatch(request()->ip(), request()->userAgent());
         return redirect()->route('main');
     }
 }
